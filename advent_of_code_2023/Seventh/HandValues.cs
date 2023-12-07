d769f6b27d9ef2cbd4c88e8a65a2501d7f7af8e0
Vetle Fongen <vetle.fongen@gmail.com>
2023-12-07 15:41:03 +0100

    Dag 7 del 1 snart ferdig, tror jeg

---
+++ advent_of_code_2023/Seventh/HandValues.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advent_of_code_2023.Seventh
+{
+    public class HandValues
+    {
+        public List<int> _cards;
+        public int _betValue;
+        public HandValues(List<int> cards, int betValue) 
+        {
+            _cards = cards;
+            _betValue = betValue;
+        }
+    }
+}
--- advent_of_code_2023/Seventh/Seventh_01.cs
+++ advent_of_code_2023/Seventh/Seventh_01.cs
@@ -12,20 +12,22 @@ namespace advent_of_code_2023.Seventh
         public Seventh_01() 
         {
             Console.WriteLine("7-1");
-            string[] allLined = File.ReadAllLines("./input7.txt");
+            string[] allLines = File.ReadAllLines("./input7.txt");
             long total = 0;
 
-            List<int> fiveOfAKind = new List<int>();
-            List<int> fourOfAKind = new List<int>();
-            List<int> fullHouse = new List<int>();
-            List<int> threeOfAKind = new List<int>();
-            List<int> twoPairs = new List<int>();
-            List<int> onePair = new List<int>();
-            List<int> highCard = new List<int>();
+            List<HandValues> handValues = new List<HandValues>();
+
+            List<HandValues> fiveOfAKind = new List<HandValues>();
+            List<HandValues> fourOfAKind = new List<HandValues>();
+            List<HandValues> fullHouse = new List<HandValues>();
+            List<HandValues> threeOfAKind = new List<HandValues>();
+            List<HandValues> twoPairs = new List<HandValues>();
+            List<HandValues> onePair = new List<HandValues>();
+            List<HandValues> highCard = new List<HandValues>();
 
             List<string> cards = new List<string>{"2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"};
 
-            foreach (string line in allLined)
+            foreach (string line in allLines)
             {
                 string[] values = line.Split(" ".ToCharArray()[0]);
 
@@ -47,14 +49,14 @@ namespace advent_of_code_2023.Seventh
                 {
                     if (matchingHand.Equals(5))
                     {
-                        fiveOfAKind = fiveOfAKind.Concat(GetValue(values[0])).ToList();
+                        fiveOfAKind.Add(new HandValues(GetValue(values[0]), int.Parse(values[1])));
                         canBeHighCard = false;
                         Console.WriteLine("Five of a kind");
                         break;
                     }
                     else if (matchingHand.Equals(4))
                     {
-                        fourOfAKind = fourOfAKind.Concat(GetValue(values[0])).ToList();
+                        fourOfAKind.Add(new HandValues(GetValue(values[0]), int.Parse(values[1])));
                         canBeHighCard = false;
                         Console.WriteLine("Four of a kind");
                         break;
@@ -98,36 +100,41 @@ namespace advent_of_code_2023.Seventh
                 }
                 if (canBeOnePair)
                 {
-                    onePair = onePair.Concat(GetValue(values[0])).ToList();
+                    onePair.Add(new HandValues(GetValue(values[0]), int.Parse(values[1])));
                     Console.WriteLine("One pair");
                 }
                 else if (canBeThreeOfAKind)
                 {
-                    threeOfAKind = threeOfAKind.Concat(GetValue(values[0])).ToList();
+                    threeOfAKind.Add(new HandValues(GetValue(values[0]), int.Parse(values[1])));
                     Console.WriteLine("Three of a kind");
                 }
                 else if (canThreeBeFullHouse || canTwoBeFullHouse)
                 {
-                    fullHouse = fullHouse.Concat(GetValue(values[0])).ToList();
+                    fullHouse.Add(new HandValues(GetValue(values[0]), int.Parse(values[1])));
                     Console.WriteLine("Full house");
                 }
                 else if (canBeTwoPairs)
                 {
-                    twoPairs = twoPairs.Concat(GetValue(values[0])).ToList();
+                    twoPairs.Add(new HandValues(GetValue(values[0]), int.Parse(values[1])));
                     Console.WriteLine("Two pairs");
                 }
                 else if (canBeHighCard)
                 {
-                    highCard = highCard.Concat(GetValue(values[0])).ToList();
+                    highCard.Add(new HandValues(GetValue(values[0]), int.Parse(values[1])));
                     Console.WriteLine("High card");
                 }
             }
-            Console.WriteLine("High card: " + highCard.Count() / 5);
+            Console.WriteLine("High card: " + highCard[0]._cards.Count() / 5);
             Console.WriteLine("Two pairs: " + twoPairs.Count() / 5);
             Console.WriteLine("Three of a kind: " + threeOfAKind.Count() / 5);
             Console.WriteLine("Full house: " + fullHouse.Count() / 5);
             Console.WriteLine("Four of a kind: " + fourOfAKind.Count() / 5);
             Console.WriteLine("Five of a kind: " + fiveOfAKind.Count() / 5);
+
+            for (int i = 0; i < fiveOfAKind.Count(); i+= 5)
+            {
+
+            }
         }
         public List<int> GetValue(string cards)
         {
